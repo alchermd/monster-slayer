344b3b9b515b512c5ca8547d4f67d8dcf8368d21
John Alcher <johnalcherdoloiras@gmail.com>
2018-12-07 13:46:21 +0800

    Can now "give up" to restart the game

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 const app = new Vue({
-    el: '#app',
-    data: {
-        gameInProgress: false
+  el: "#app",
+  data: {
+    gameInProgress: false
+  },
+  methods: {
+    startGame() {
+      this.gameInProgress = true;
     },
+    giveUp() {
+      this.gameInProgress = false;
+    }
+  }
 });
